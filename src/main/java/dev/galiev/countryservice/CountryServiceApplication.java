5985e4de89819a0b84ee782563b0669d7ea53591
galiev <ggvalion@gmail.com>
2023-11-05 10:16:36 +0300

    Cross mapping

--- src/main/java/dev/galiev/countryservice/CountryServiceApplication.java
+++ src/main/java/dev/galiev/countryservice/CountryServiceApplication.java
@@ -2,12 +2,25 @@ package dev.galiev.countryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CountryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CountryServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
--- src/main/java/dev/galiev/countryservice/controllers/CountriesController.java
+++ src/main/java/dev/galiev/countryservice/controllers/CountriesController.java
@@ -4,13 +4,10 @@ import dev.galiev.countryservice.dto.CountryInfo;
 import dev.galiev.countryservice.model.countries.Countries;
 import dev.galiev.countryservice.service.CountriesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("countries")
+@RequestMapping("countries/")
 public class CountriesController {
 
     @Autowired
@@ -21,7 +18,7 @@ public class CountriesController {
         return service.getCountries();
     }
 
-    @GetMapping("/{name}")
+    @GetMapping("{name}")
     public CountryInfo country(@PathVariable String name) {
         return service.getCountryInfoByName(name);
     }
